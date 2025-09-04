96e4522a270c73bde05d709d914b4711da4ae78b
d3sp1iR <relersad@gmail.com>
2025-09-04 21:27:38 +0300

    Создана сама слот машина, проект собран. версия 0.1

---
+++ src/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, ""); //ит ис фор рашн, андерстэнд? на всякий пожарный
+    srand(time(0)); // инициализатор рандома для слотиков
+
+    vector<string> symbols = {"🍒", "🍋", "🔔", "7"};
+
+    cout << "=== Слот-машина ===" << endl;
+
+    char play;
+    do {
+        vector<string> result(3);
+        
+        for (int i = 0; i < 3; i++) {
+            int index = rand() % symbols.size();
+            result[i] = symbols[index];
+
+        }
+
+        //сами слоты
+        cout << "| " << result[0] << " | " << result[1] << " | " << result[2] << " |" << endl;
+
+        if (result[0] == result[1] && result[1] == result[2]) {
+            cout << "Конгратилэйшнс, ю пробабли вин нафинг!!! :)" << endl;
+        }
+        else {
+            cout << "Бэд трай, лузер. Нужен додеп" << endl;
+        }
+
+        cout << "Ду ю вона трай додеп? (y/n)";
+        cin >> play;
+        
+    } while (play == 'y' || play == 'Y');
+
+    cout << "Денежки кончились? тогда аувидерзейн" << endl;
+
+    return 0;
+
+
+}
