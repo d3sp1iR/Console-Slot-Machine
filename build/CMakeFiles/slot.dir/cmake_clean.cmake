0a8cb437b5dfc9a8ebaaabb5d5a647930bc49313
d3sp1iR <relersad@gmail.com>
2025-09-04 21:22:02 +0300

    Создана сама слот машина, проект собран. версия 0.1

---
+++ build/CMakeFiles/slot.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/slot.dir/src/main.cpp.o"
+  "CMakeFiles/slot.dir/src/main.cpp.o.d"
+  "slot"
+  "slot.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/slot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
